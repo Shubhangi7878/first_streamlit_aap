acc6349221994a02e14bd1793ce8a18fb4ee03c3
Shubhangi Pansare <119045639+Shubhangi7878@users.noreply.github.com>
2022-11-30 15:00:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & bluberry Oatmeal')
 streamlit.text('Kale, snipach @ rocket smothie')
 streamlit.text('Hard-boild free-range egg')
+streamlit.text('Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
